ee61ec958f000c7a8bfec6c7d36ab592d5e052be
quangvan99 <vanquang.tpa@gmail.com>
2024-05-05 21:33:08 +0700

    update main.py print

--- assessment_II/main.py
+++ assessment_II/main.py
@@ -9,10 +9,10 @@ def find_a_feasible_path(file):
     tsp = BaseTSP()
     m = create_matrix_csv(file)
     tsp.set_matrix(create_weighted_matrix(m))
-    best = tsp.search_path(fnext=tsp.next_with_threshold, k=len(m), init_tour=[0], threshold=0)
+    path = tsp.search_path(fnext=tsp.next_with_threshold, k=len(m), init_tour=[0], threshold=0)
     with open(file.replace('input', 'output').replace('csv', 'txt'), "w") as f:
-        f.write("           Path : " + "-".join([str(i) for i in best]) + '\n')
-        f.write("Total distances : " + f"{calc_distance(m, best):.1f}")
+        f.write("           Path : " + "-".join([str(i) for i in path]) + '\n')
+        f.write("Total distances : " + f"{calc_distance(m, path):.1f}")
 
 # (for solve_atsp)
 def solve_atsp(file, method, f_heuristic = None):
@@ -29,14 +29,15 @@ if __name__ == "__main__":
         find_a_feasible_path(f"input/I{i}.csv")
 
     # (for solve atsp)
-    # methods = [MyTSP, EvolutionaryTSP, AntColonyTSP]
-    # otp_for = [calc_distance, calc_custom]
-    # method, f_heuristic = methods[0], otp_for[1]
-    # print("---", method.__name__, "---")
-    # for i in [20, 30, 40, 50, 60]:
-    #     file=f"input/I{i}.csv"
-    #     t1 = time.time()
-    #     bests = [solve_atsp(file,method,f_heuristic) for _ in range(5)]
-    #     t2 = time.time()
-    #     print(f"Time of {file}:", (t2-t1)/5)
-    #     print(f"Best of {file}:", min(bests))
+    methods = [MyTSP, EvolutionaryTSP, AntColonyTSP]
+    otp_for = [calc_distance, calc_custom]
+    method, f_heuristic = methods[0], otp_for[1]
+    print("---", method.__name__, "---")
+    for i in [20, 30, 40, 50, 60]:
+        file=f"input/I{i}.csv"
+        t1 = time.time()
+        bests = [solve_atsp(file,method,f_heuristic) for _ in range(5)]
+        t2 = time.time()
+        print(f"Time execute {file}:", (t2-t1)/5)
+        print(f"Best solution of {file}:", min(bests))
+        print("Time excu")
--- assessment_II/utils/models.py
+++ assessment_II/utils/models.py
@@ -29,67 +29,67 @@ class BaseTSP:
         thresholds = [threshold for threshold in np.linspace(values[j_low], values[j_up], n_population)]
         return thresholds
 
-    def local_search(self, tour, start_node=4):
+    def local_search(self, path, start_node=4):
         """
-        Perform local search on the given tour using 2-opt algorithm.
+        Perform local search on the given path using 2-opt algorithm.
 
         Parameters:
-        - tour: list
-            List representing the tour.
+        - path: list
+        List representing the path.
         - start_node: int
             Index of the starting node for local search.
 
         Returns:
-        Optimized tour after local search.
+        Optimized path after local search.
         """
-        stable, best = False, self.heuristic(self.m, tour)
+        stable, best = False, self.heuristic(self.m, path)
         while not stable:
             stable = True
             for i in range(start_node, self.n):
                 for j in range(i + 1, self.n+1):
-                    candidate = tour[:]
+                    candidate = path[:]
                     candidate[i], candidate[j] = candidate[j], candidate[i]
                     sub = candidate[i-1:j+2]
                     if not np.all(self.m[sub[:-1], sub[1:]] != 0):
                         continue
                     length_candidate = self.heuristic(self.m, candidate)
                     if best > length_candidate:
-                        tour, best = candidate, length_candidate
+                        path, best = candidate, length_candidate
                         stable = False
-        return tour
+        return path
 
     def next_with_threshold(self, **kwargs):
         """Choose the next node based on threshold."""
-        cur, threshold, nexts, tour  = kwargs['cur'], kwargs['threshold'], kwargs['nexts'], kwargs['tour']
+        cur, threshold, nexts, path  = kwargs['cur'], kwargs['threshold'], kwargs['nexts'], kwargs['path']
         costs = np.abs(self.m[cur, nexts] - threshold)
         j_min = np.argmin(costs)
         next = nexts[j_min]
-        tour.append(next)
-        return next, tour
+        path.append(next)
+        return next, path
 
     def next_with_prob_threshold(self, **kwargs):
         """Choose the next node based on threshold."""
-        cur, threshold, nexts, tour  = kwargs['cur'], kwargs['threshold'], kwargs['nexts'], kwargs['tour']
+        cur, threshold, nexts, path  = kwargs['cur'], kwargs['threshold'], kwargs['nexts'], kwargs['path']
         costs = np.abs(self.m[cur, nexts] - threshold)
         idxs = np.argsort(costs)[:3]
         distances = costs[idxs]
         probs = distance_to_probability(distances)
         j_min = choices(idxs, weights=probs)[0]
         next = nexts[j_min]
-        tour.append(next)
-        return next, tour
+        path.append(next)
+        return next, path
 
 
     def next_with_heuristic(self, **kwargs):
         """Select the next node based on cost."""
-        nexts, tour = kwargs['nexts'], kwargs['tour']
-        costs = [self.heuristic(self.m, tour + [next]) for next in nexts]
+        nexts, path = kwargs['nexts'], kwargs['path']
+        costs = [self.heuristic(self.m, path + [next]) for next in nexts]
         j_min = np.argmin(costs)
         next = nexts[j_min]
-        tour.append(next)
-        return next, tour
+        path.append(next)
+        return next, path
 
-    def search_path(self, fnext, k, init_tour=[0], **kwargs):
+    def search_path(self, fnext, k, init_path=[0], **kwargs):
         """
         Search for a path through the TSP using a specified function.
 
@@ -98,35 +98,35 @@ class BaseTSP:
             Function to use for selecting the next node.
         - k: int
             Number of nodes in the path.
-        - init_tour: list, optional
-            Initial tour. Default is [0].
+        - init_path: list, optional
+        Initial path. Default is [0].
 
         Returns:
         Path through the TSP.
         """
-        cur, tour = init_tour[-1], init_tour
+        cur, path = init_path[-1], init_path
         forbidden = defaultdict(list)
         count = 0
-        while len(tour) < k:
-            _k = tuple(tour) # Convert tour to a tuple for easy indexing
+        while len(path) < k:
+            _k = tuple(path) # Convert path to a tuple for easy indexing
             nexts = np.argwhere(self.m[cur, :] != 0).flatten()
             nexts = [next for next in nexts
-                    if (next not in tour and next != self.n+1 and next not in forbidden[_k])
-                    or (len(tour) == self.n+1 and next == self.n+1)]
+                    if (next not in path and next != self.n+1 and next not in forbidden[_k])
+                    or (len(path) == self.n+1 and next == self.n+1)]
 
             # If there are no valid neighbors
             if len(nexts) <= 0:
-                cur = tour[-2] # Move back to the previous node
-                forbidden[_k[:-1]].append(tour[-1])
-                tour = tour[:-1]
+                cur = path[-2] # Move back to the previous node
+                forbidden[_k[:-1]].append(path[-1])
+                path = path[:-1]
                 count += 1
-                if count > 1000: return tour # Prevent infinite loops
+                if count > 1000: return path # Prevent infinite loops
                 continue
             kwargs['nexts']=nexts
-            kwargs['tour']=tour
+            kwargs['path']=path
             kwargs['cur']=cur
-            cur, tour = fnext(**kwargs)
-        return tour
+            cur, path = fnext(**kwargs)
+        return path
 
     def fit(self):
         pass
@@ -146,12 +146,12 @@ class MyTSP(BaseTSP):
         Initialize the heads for the search.
 
         Returns:
-        - List of heads (list): List of initial tour heads.
+        - List of heads (list): List of initial path heads.
         """
         self.k_head = int(self.n*self.k_percent) # the number of initial vertices traversed
         thresholds = self.get_threshold(self.m)
         heads = [self.search_path(fnext=self.next_with_prob_threshold, k=self.k_head,
-                                    init_tour=[0], threshold=t) for t in thresholds]
+                                    init_path=[0], threshold=t) for t in thresholds]
         heads = list(set(tuple(head) for head in heads))
         return heads
 
@@ -159,10 +159,10 @@ class MyTSP(BaseTSP):
         heads = self.initialize()
         result = []
         for head in heads:
-            tour = self.search_path(fnext=self.next_with_heuristic, k=self.n+2, init_tour=list(head))
-            if tour[-1] != self.n+1: continue
-            new_tour = self.local_search(tour, start_node=self.k_head)
-            result.append([self.heuristic(self.m, new_tour), new_tour])
+            path = self.search_path(fnext=self.next_with_heuristic, k=self.n+2, init_path=list(head))
+            if path[-1] != self.n+1: continue
+            new_path = self.local_search(path, start_node=self.k_head)
+            result.append([self.heuristic(self.m, new_path), new_path])
         result = min(result)
         return result
 
@@ -171,13 +171,13 @@ class EvolutionaryTSP(BaseTSP):
     def __init__(self, heuristic,
                     n_population=100,
                     n_epoch=100,
-                    tournament_size=4,
+                    pathnament_size=4,
                     mutation_rate=0.5,
                     crossover_rate=0.9):
         super().__init__(heuristic)
         self.n_population = n_population
         self.n_epoch = n_epoch
-        self.tournament_size = tournament_size
+        self.pathnament_size = pathnament_size
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
 
@@ -191,10 +191,10 @@ class EvolutionaryTSP(BaseTSP):
         thresholds = self.get_threshold(self.m, self.n_population)
         population = []
         for threshold in thresholds:
-            tour = self.search_path(fnext=self.next_with_threshold, k=self.n+2,
-                                    init_tour=[0], threshold=threshold)
-            if tour[-1] != self.n+1: continue
-            population.append([self.heuristic(self.m, tour), tour])
+            path = self.search_path(fnext=self.next_with_threshold, k=self.n+2,
+                                    init_path=[0], threshold=threshold)
+            if path[-1] != self.n+1: continue
+            population.append([self.heuristic(self.m, path), path])
         if len(population) < self.n_population:
             population = population + choices(population, k=self.n_population-len(population))
         return population
@@ -267,8 +267,8 @@ class EvolutionaryTSP(BaseTSP):
                 # CROSSOVER
                 random_number = random()
                 if random_number < self.crossover_rate:
-                    parent1 = sorted(choices(population, k=self.tournament_size))[0][1]
-                    parent2 = sorted(choices(population, k=self.tournament_size))[0][1]
+                    parent1 = sorted(choices(population, k=self.pathnament_size))[0][1]
+                    parent2 = sorted(choices(population, k=self.pathnament_size))[0][1]
                     k = randint(1, self.n)
                     child1 = self.cross_over(parent1, parent2, k)
                     child2 = self.cross_over(parent2, parent1, k)
@@ -327,7 +327,7 @@ class AntColonyTSP(BaseTSP):
         """
         thresholds = self.get_threshold(self.m, self.n_ant)
         heads = [self.search_path(fnext=self.next_with_threshold, k=self.k,
-                                    init_tour=[0], threshold=t) for t in thresholds]
+                                    init_path=[0], threshold=t) for t in thresholds]
         pheromones = np.zeros((self.n+2, self.n+2))
         for head in heads:
             for x, y in zip(head[:-1], head[1:]):
@@ -354,20 +354,20 @@ class AntColonyTSP(BaseTSP):
         Choose the next node based on pheromones.
 
         Parameters:
-            **kwargs: Arbitrary keyword arguments containing 'cur', 'tour', 'nexts', and 'pheromones'.
+            **kwargs: Arbitrary keyword arguments containing 'cur', 'path', 'nexts', and 'pheromones'.
 
         Returns:
-            tuple: Tuple containing the next node and updated tour.
+            tuple: Tuple containing the next node and updated path.
         """
-        cur, tour, nexts, pheromones = kwargs["cur"], kwargs["tour"], kwargs["nexts"], kwargs["pheromones"]
-        costs = np.array([1/self.heuristic(self.m, tour + [next]) for next in nexts])
+        cur, path, nexts, pheromones = kwargs["cur"], kwargs["path"], kwargs["nexts"], kwargs["pheromones"]
+        costs = np.array([1/self.heuristic(self.m, path + [next]) for next in nexts])
         next_prob = self.calc_prob(pheromones[cur, nexts], costs, self.alpha, self.beta)
         j_min = np.argmax(next_prob)
         next = nexts[j_min]
-        tour.append(next)
+        path.append(next)
         pheromones[cur, next] += self.del_tau
         cur = next
-        return next, tour
+        return next, path
 
     def fit(self):
         assert self.m is not None, "Matrix not set"
@@ -376,19 +376,19 @@ class AntColonyTSP(BaseTSP):
         elitist_epochs = []
         for epoch in range(self.n_epoch):
             elitist_ants = []
-            for init_tour in heads:
-                nexts = np.argwhere(self.m[init_tour[-1], :] != 0).flatten()
-                next = choice([next for next in nexts if next not in init_tour])
-                init_tour = init_tour + [next]
-                tour = self.search_path(fnext=self.next_with_pheromones, k=self.n+2,
-                                init_tour=init_tour, pheromones=pheromones)
-                if tour[-1] != self.n+1: continue
-                elitist_ants.append([self.heuristic(self.m, tour), tour])
+            for init_path in heads:
+                nexts = np.argwhere(self.m[init_path[-1], :] != 0).flatten()
+                next = choice([next for next in nexts if next not in init_path])
+                init_path = init_path + [next]
+                path = self.search_path(fnext=self.next_with_pheromones, k=self.n+2,
+                                init_path=init_path, pheromones=pheromones)
+                if path[-1] != self.n+1: continue
+                elitist_ants.append([self.heuristic(self.m, path), path])
 
             # Update elite pheromones
             elitist = min(elitist_ants)
             if self.is_local_search:
-                elitist=self.local_search(tour=elitist[1], start_node=self.k+1)
+                elitist=self.local_search(path=elitist[1], start_node=self.k+1)
                 elitist = [self.heuristic(self.m, elitist), elitist]
             path = elitist[1]
             for i in range(self.n+1):
